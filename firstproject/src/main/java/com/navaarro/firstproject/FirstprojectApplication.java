55e34cddfb858a0270bb33803662ab3ee7fa7a11
Brayan Navarro <brayannavarro209@gmail.com>
2021-08-04 18:25:32 -0700

    StringsAssignment

---
+++ firstproject/src/main/java/com/navaarro/firstproject/FirstprojectApplication.java
@@ -0,0 +1,25 @@
+package com.navaarro.firstproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class FirstprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstprojectApplication.class, args);
+	}
+
+
+	@RequestMapping("/")
+	public String hello() {
+		return "Hello Client! How ar you doing?";
+	}
+	@RequestMapping("/random")
+	public String boot() {
+		return "Spring Boot is great! So easy to just respod with strings";
+	}
+}
